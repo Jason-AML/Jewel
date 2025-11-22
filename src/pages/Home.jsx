e23dcbfcdb7f209db47e0715dd1964c28ebf4ab2
jason <jaml08@hotmail.com>
2025-11-22 18:49:43 -0500

    feat:Agregue un loader mientras los datos del hooks useQuery esta cargando los datos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import { Hero } from "../components/Hero";
 import { LayoutApp } from "../structure/LayoutApp";
 import { useQuery } from "../hooks/useQuery";
 import { FilteredProduct } from "../components/filtered/FilteredProduct";
+import { Loader } from "../components/loader/Loader";
 export const Home = () => {
   const { data, loading, error } = useQuery({
     url: "https://fakestoreapi.com/products",
   });
   console.log(data);
-  if (loading) return <p>cargando...</p>;
+  if (loading) return <Loader fullscreen={true} />;
   if (error) return <p>error: {error}</p>;
   return (
     <LayoutApp>
