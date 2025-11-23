3b7ae1852cfd8ce5376ae641b1fed6cdc1dbb6b8
jason <jaml08@hotmail.com>
2025-11-22 22:32:50 -0500

    feat: Datos para que el componente reutilizable Hero renderice

---
+++ src/data/heroData/Data.js
@@ -0,0 +1,43 @@
+import collarGold from "../../assets/hero/joya.jpg";
+import ringGirl from "../../assets/hero/girl.webp";
+export const joyasMockup = [
+  {
+    id: "JYA001",
+    nombre: "Anillo Solitario 'Aurora'",
+    precio: 750.99,
+    material: "Oro Blanco de 18k",
+    piedraPrincipal: "Diamante (Corte Brillante)",
+    quilates: 0.75,
+    talla: "Estándar",
+    disponible: true,
+    image: collarGold,
+    descripcion:
+      "Un anillo de compromiso clásico que destaca por su diseño minimalista y el brillo excepcional de su diamante central.",
+  },
+  {
+    id: "JYC002",
+    nombre: "Collar 'Cielo Estrellado'",
+    precio: 299.5,
+    material: "Plata Esterlina 925",
+    piedraPrincipal: "Zafiros Azules",
+    longitudCM: 45,
+    tipoCierre: "Mosquetón",
+    disponible: true,
+    image: ringGirl,
+    descripcion:
+      "Delicado collar con pequeños zafiros incrustados que imitan un cielo nocturno. Perfecto para uso diario o eventos discretos.",
+  },
+  {
+    id: "JYP003",
+    nombre: "Pendientes 'Gota de Perla'",
+    precio: 450.0,
+    material: "Oro Amarillo de 14k",
+    piedraPrincipal: "Perla de Agua Dulce",
+    pesoGramos: 4.2,
+    tipoSujecion: "Enganche",
+    disponible: false,
+    image: ringGirl,
+    descripcion:
+      "Elegantes pendientes con una perla de forma perfecta suspendida de un gancho de oro. Añaden un toque de sofisticación atemporal.",
+  },
+];
