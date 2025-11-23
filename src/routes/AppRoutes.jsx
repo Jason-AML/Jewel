2d4b8e2c167d6f93c1e8a0db6a472f1959ac2f1d
jason <jaml08@hotmail.com>
2025-11-22 22:38:15 -0500

    feat: agregue nueva ruta para los productos

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Profile } from "../pages/Profile";
+import { Product } from "../pages/Products";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
